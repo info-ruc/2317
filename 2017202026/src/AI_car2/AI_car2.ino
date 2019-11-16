f82b70122fcd0c887ca451db6cfa018429c8197e
WangYuanZhen123 <2017202026@ruc.edu.cn>
2019-11-17 00:20:15 +0800

    description of edit

--- 2017202026/src/AI_car2/AI_car2.ino
+++ 2017202026/src/AI_car2/AI_car2.ino
@@ -1,26 +1,40 @@
+#include <Servo.h>
+
 #define STOP      0
 #define FORWARD   1
 #define BACKWARD  2
 #define TURNLEFT  3
 #define TURNRIGHT 4
 
-int leftMotor1 = 4;
-int leftMotor2 = 5;
+int leftMotor1 = 8;
+int leftMotor2 = 9;
 int rightMotor1 = 6;
 int rightMotor2 = 7;
 
+
+Servo myServo;  //舵机
+
+int inputPin=10;   // 定义超声波信号接收接口
+int outputPin=11;  // 定义超声波信号发出接口
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  //串口初始化
+  Serial.begin(9600); 
+  //舵机引脚初始化
+  myServo.attach(9);
+  //测速引脚初始化
   pinMode(leftMotor1, OUTPUT);
   pinMode(leftMotor2, OUTPUT);
   pinMode(rightMotor1, OUTPUT);
   pinMode(rightMotor2, OUTPUT);
+  //超声波控制引脚初始化
+  pinMode(inputPin, INPUT);
+  pinMode(outputPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //usart read
   if(Serial.available()>0)
   {
     char cmd = Serial.read();//读取蓝牙模块发送到串口的数据
@@ -39,6 +53,7 @@ void motorRun(int cmd)
       digitalWrite(leftMotor2, LOW);
       digitalWrite(rightMotor1, HIGH);
       digitalWrite(rightMotor2, LOW);
+      avoidance();
       break;
      case BACKWARD:
       Serial.println("BACKWARD"); //输出状态
@@ -46,20 +61,23 @@ void motorRun(int cmd)
       digitalWrite(leftMotor2, HIGH);
       digitalWrite(rightMotor1, LOW);
       digitalWrite(rightMotor2, HIGH);
+      avoidance();
       break;
      case TURNLEFT:
       Serial.println("TURN  LEFT"); //输出状态
       digitalWrite(leftMotor1, HIGH);
       digitalWrite(leftMotor2, LOW);
       digitalWrite(rightMotor1, LOW);
-      digitalWrite(rightMotor2, HIGH);
+      digitalWrite(rightMotor2, LOW);
+      avoidance();
       break;
      case TURNRIGHT:
       Serial.println("TURN  RIGHT"); //输出状态
       digitalWrite(leftMotor1, LOW);
-      digitalWrite(leftMotor2, HIGH);
+      digitalWrite(leftMotor2, LOW);
       digitalWrite(rightMotor1, HIGH);
       digitalWrite(rightMotor2, LOW);
+      avoidance();
       break;
      default:
       Serial.println("STOP"); //输出状态
@@ -67,5 +85,37 @@ void motorRun(int cmd)
       digitalWrite(leftMotor2, LOW);
       digitalWrite(rightMotor1, LOW);
       digitalWrite(rightMotor2, LOW);
+      avoidance();
   }
 }
+void avoidance()
+{
+  int dis;//距离
+  //motorRun(FORWARD);
+  dis=getDistance(); //中间
+  
+  if(dis<30)
+  {
+    motorRun(TURNRIGHT);
+  }
+}
+int getDistance()
+{
+  digitalWrite(outputPin, LOW); // 使发出发出超声波信号接口低电平2μs
+  delayMicroseconds(2);
+  digitalWrite(outputPin, HIGH); // 使发出发出超声波信号接口高电平10μs，这里是至少10μs
+  delayMicroseconds(10);
+  digitalWrite(outputPin, LOW); // 保持发出超声波信号接口低电平
+  int distance = pulseIn(inputPin, HIGH); // 读出脉冲时间
+  distance= distance/58; // 将脉冲时间转化为距离（单位：厘米）
+  Serial.println(distance); //输出距离值
+ 
+  if (distance >=50)
+  {
+    //如果距离小于50厘米返回数据
+    return 50;
+  }//如果距离小于50厘米小灯熄灭
+  else
+    return distance;
+}
+
